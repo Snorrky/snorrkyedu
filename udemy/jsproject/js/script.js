b79db6e6dd9d6eb314182528e669c04cb86ac651
Anastasy <snorrky@gmail.com>
2021-05-15 12:18:24 +0300

    add js

---
+++ udemy/jsproject/js/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+}
+
+obj.a = 10;
+
+console.log(obj);
+
+var name = 'Ivan';
+
+{
+    let result = 50;
+}
+
+//const result = confirm("Are you here?");
+
+//const result = prompt("Are you 18?", "yes");
+
+const result = "may";
+
+// result[0] = prompt("your name?", "");
+// result[1] = prompt("your age?", "");
+// result[2] = prompt("your sex?", "");
+
+// document.write(result);
+
+console.log(`текущи месяц ${result}`);
+
